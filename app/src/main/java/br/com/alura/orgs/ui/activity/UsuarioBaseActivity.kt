df21d28c46d3145961aeb35f981e2d53970e411f
Alex Felipe <alexfelipevieira@gmail.com>
2021-12-21 09:41:25 -0300

    5.1

--- app/src/main/java/br/com/alura/orgs/ui/activity/UsuarioBaseActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/UsuarioBaseActivity.kt
@@ -18,8 +18,8 @@ abstract class UsuarioBaseActivity : AppCompatActivity() {
     private val usuarioDao by lazy {
         AppDatabase.instancia(this).usuarioDao()
     }
-    private var _usuario: MutableStateFlow<Usuario?> = MutableStateFlow(null)
-    protected var usuario: StateFlow<Usuario?> = _usuario
+    private val _usuario: MutableStateFlow<Usuario?> = MutableStateFlow(null)
+    protected val usuario: StateFlow<Usuario?> = _usuario
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +36,12 @@ abstract class UsuarioBaseActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun buscaUsuario(usuarioId: String) {
-            _usuario.value = usuarioDao
-                .buscaPorId(usuarioId)
-                .firstOrNull()
+    private suspend fun buscaUsuario(usuarioId: String): Usuario? {
+        return usuarioDao
+            .buscaPorId(usuarioId)
+            .firstOrNull().also {
+                _usuario.value = it
+            }
     }
 
     protected suspend fun deslogaUsuario() {
